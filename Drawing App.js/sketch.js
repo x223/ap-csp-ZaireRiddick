d1c318d238fe9af5e024c10e5f83a618545fb804
ZaireRiddick <zaire.riddick@ms223.org>
2016-10-17 15:18:48 -0400

    Add files via upload

---
+++ Drawing App.js/sketch.js
@@ -0,0 +1,31 @@
+var strokeColor = 'black';
+
+function keyTyped() {
+	if (key === 'b') {
+		strokeColor = 'black';
+	} else if (key === 'p') {
+		strokeColor = '#E32173';
+		weight = 10;
+	}
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	if (mouseIsPressed) {
+		stroke(strokeColor)
+		line(pmouseX, pmouseY, mouseX, mouseY);
+	}
+}
+
+function keyTyped() {
+	if (key === 'b') {
+		strokeColor = 'black';
+	} else if (key === 'p') {
+		strokeColor = '#E32173';
+		weight = 10;
+	}
+	else if (key ==='p') {
+}
